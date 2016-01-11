bca7fd72ef7b7ab3208161eb224007b29da12727
llengg <lulengling@gmail.com>
2016-01-11 10:05:21 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 echo "root:$ROOT_PASS" | chpasswd
 
-service vncserver restart
-sed -i "s/x-window-manager/gnome-session/g" /root/.vnc/xstartup
+sed -i "s/x-www-browser/\/opt\/firefox\/firefox/g" /etc/xdg/openbox/menu.xml
+sed -i "s/x-terminal-emulator/\/usr\/bin\/lantern/g" /etc/xdg/openbox/menu.xml
+
+service vncserver start
+sed -i "s/x-terminal-emulator/#x-terminal-emulator/g" /root/.vnc/xstartup
+sed -i "s/x-window-manager/#x-window-manager/g" /root/.vnc/xstartup
 
 exec /usr/sbin/sshd -D

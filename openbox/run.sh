9278c5df994623356eb177297481f05f753523ec
llengg <lulengling@gmail.com>
2016-01-22 11:20:13 +0800

    Update run.sh

--- openbox/run.sh
+++ openbox/run.sh
@@ -2,4 +2,20 @@
 
 echo "root:$ROOT_PASS" | chpasswd
 
+sed -i "s/x-www-browser/winecfg/g" /etc/xdg/openbox/menu.xml
+
+cat > /root/.vnc/xstartup <<EOF
+#!/bin/sh
+[ -x /etc/vnc/xstartup ] && exec /etc/vnc/xstartup
+[ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
+xsetroot -solid grey
+vncconfig -iconic &
+#x-terminal-emulator -geometry 80x24+10+10 -ls -title "$VNCDESKTOP Desktop" &
+#x-window-manager &
+tint2 -c /etc/xdg/tint2/tint2rc &
+rox-filer &
+openbox-session &
+EOF
+chmod 755 /root/.vnc/xstartup
+
 exec /usr/sbin/sshd -D

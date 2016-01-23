22976722f1c502d3a8d495485c8287d087c71f6b
llengg <lulengling@gmail.com>
2016-01-23 14:13:28 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,19 +4,19 @@ echo "root:$ROOT_PASS" | chpasswd
 sed -i "s/x-www-browser/\/opt\/firefox\/firefox/g" /etc/xdg/openbox/menu.xml
 sed -i "s/x-terminal-emulator/\/usr\/bin\/lantern/g" /etc/xdg/openbox/menu.xml
 
-cat > /root/.vnc/xstartup <<EOF
-#!/bin/sh
-[ -x /etc/vnc/xstartup ] && exec /etc/vnc/xstartup
-[ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
-xsetroot -solid grey
-vncconfig -iconic &
+#cat > /root/.vnc/xstartup <<EOF
+##!/bin/sh
+#[ -x /etc/vnc/xstartup ] && exec /etc/vnc/xstartup
+#[ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
+#xsetroot -solid grey
+#vncconfig -iconic &
 #x-terminal-emulator -geometry 80x24+10+10 -ls -title "$VNCDESKTOP Desktop" &
 #x-window-manager &
-tint2 -c /etc/xdg/tint2/tint2rc &
+#tint2 -c /etc/xdg/tint2/tint2rc &
 #rox-filer &
-openbox-session &
-EOF
-chmod 755 /root/.vnc/xstartup
+#openbox-session &
+#EOF
+#chmod 755 /root/.vnc/xstartup
 
 cat > /root/astart <<EOF
 #!/bin/bash

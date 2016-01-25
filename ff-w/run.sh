6caaf7f64d8dfaf5f61a55eaf8695db6c728b9df
llengg <lulengling@gmail.com>
2016-01-25 15:02:31 +0800

    Update run.sh

--- ff-w/run.sh
+++ ff-w/run.sh
@@ -4,13 +4,24 @@ echo "root:$ROOT_PASS" | chpasswd
 sed -i "s/x-www-browser/\/opt\/firefox\/firefox/g" /etc/xdg/openbox/menu.xml
 sed -i "s/x-terminal-emulator/\/usr\/bin\/lantern/g" /etc/xdg/openbox/menu.xml
 
-#cat > /root/ff <<EOF
-##!/bin/bash
-#while true; do
-#[ -z "`ps -ef | grep firefox| grep -v grep`" ] && export DISPLAY=0:1 && nohup /opt/firefox/firefox > /dev/null 2>&1 &
-#sleep 5m
-#done
-#EOF
+cat > /root/ff <<EOF
+#!/bin/bash
+while true; do
+[ -z "`ps -ef | grep firefox| grep -v grep`" ] && export DISPLAY=0:1 && nohup /opt/firefox/firefox > /dev/null 2>&1 &
+sleep 5m
+done
+EOF
+
+cat > /root/xstartup <<EOF
+#!/bin/sh
+[ -x /etc/vnc/xstartup ] && exec /etc/vnc/xstartup
+[ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
+xsetroot -solid grey
+vncconfig -iconic &
+tint2 -c /etc/xdg/tint2/tint2rc &
+thunar &
+openbox-session &
+EOF
 
 echo -e 'MAILTO=gaoal@dagene.net\n59 13 * * * kill $(pgrep firefox) ; sleep 3s ; rm -rf $(find ~ -name sessionstore.js)' \
         > /var/spool/cron/crontabs/root

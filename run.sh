c3f8965f4bfbf563daa19c63c41c8768dc3c3d31
llengg <lulengling@gmail.com>
2016-02-02 15:43:57 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,19 +4,15 @@ echo "root:$ROOT_PASS" | chpasswd
 sed -i "s/x-www-browser/\/opt\/firefox\/firefox/g" /etc/xdg/openbox/menu.xml
 sed -i "s/x-terminal-emulator/\/usr\/bin\/lantern/g" /etc/xdg/openbox/menu.xml
 
-#cat > /root/.vnc/xstartup <<EOF
-##!/bin/sh
-#[ -x /etc/vnc/xstartup ] && exec /etc/vnc/xstartup
-#[ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
-#xsetroot -solid grey
-#vncconfig -iconic &
-#x-terminal-emulator -geometry 80x24+10+10 -ls -title "$VNCDESKTOP Desktop" &
-#x-window-manager &
-#tint2 -c /etc/xdg/tint2/tint2rc &
-#rox-filer &
-#openbox-session &
-#EOF
-#chmod 755 /root/.vnc/xstartup
+cat > /root/xstartup <<EOF
+#!/bin/sh
+[ -x /etc/vnc/xstartup ] && exec /etc/vnc/xstartup
+[ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
+xsetroot -solid grey
+vncconfig -iconic &
+tint2 -c /etc/xdg/tint2/tint2rc &
+openbox-session &
+EOF
 
 cat > /root/astart <<EOF
 #!/bin/bash
@@ -25,12 +21,10 @@ while true; do
 sleep 5m
 done
 EOF
-#nohup bash /root/astart &
 
 echo -e 'MAILTO=gaoal@dagene.net\n59 13 * * * kill $(pgrep firefox) ; sleep 3s ; rm -rf $(find ~ -name sessionstore.js)' \
         > /var/spool/cron/crontabs/root
 chmod 600 /var/spool/cron/crontabs/root
 chown root.crontab /var/spool/cron/crontabs/root
-#service cron restart
 
 exec /usr/sbin/sshd -D

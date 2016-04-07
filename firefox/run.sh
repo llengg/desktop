88321d7d131f56596eae8c4f86fac29dbe927025
llengg <lulengling@gmail.com>
2016-04-07 13:55:29 +0800

    Update run.sh

--- firefox/run.sh
+++ firefox/run.sh
@@ -18,12 +18,12 @@ EOF
 cat > /root/astart <<EOF
 #!/bin/bash
 while true; do
-for (( i=0; i<96; i++ ))
+for (( i=0; i<72; i++ ))
 {
 sleep 5m
-[ -z "`ps -ef | grep firefox| grep -v grep`" ] && export DISPLAY=0:1 && nohup /opt/firefox/firefox & > /dev/null 2>&1
+[ -z "\`ps -ef | grep firefox| grep -v grep\`" ] && export DISPLAY=0:1 && nohup /opt/firefox/firefox & > /dev/null 2>&1
 }
-pkill -9 firefox ; rm -rf $(find ~ -name sessionstore.js)
+pkill -9 firefox ; rm -rf \$(find ~ -name sessionstore.js)
 date -R >> /root/log
 done
 EOF
